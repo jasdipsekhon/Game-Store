fb5c2789f3cf3e0d4dbcc3629fe0a6e5fdb86df7
jasdipsekhon <jasdipsekhon@gmail.com>
2020-01-21 00:40:14 -0800

    Create FindFpsTester.h

---
+++ FindFpsTester.h
@@ -0,0 +1,21 @@
+//
+//  FindFpsTester.h
+//  rick'sGames
+//
+//  Created by Jasdip Sekhon on 1/20/20.
+//  Copyright © 2020 Jasdip Sekhon. All rights reserved.
+//
+
+#ifndef FindFpsTester_h
+#define FindFpsTester_h
+
+#include "Inventory.h"
+using namespace std;
+
+class FindFpsTester
+{
+public:
+    static void initializeInventory(Inventory *inventory);
+};
+
+#endif /* FindFpsTester_h */
